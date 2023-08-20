9f6aeeecc819b3eee70503858b0e1cb2fcba64b9
MaBling <akck0918@gmail.com>
2023-08-21 01:18:17 +0900

    Change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-  "dracula/vim",
+  "Mofiqul/vscode.nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
     -- load the colorscheme here
-    vim.cmd([[colorscheme dracula]])
+    require("vscode").load()
   end,
 }
