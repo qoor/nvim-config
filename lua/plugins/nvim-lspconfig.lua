34e20df369fa53b7d4dc779f058af7f99105300a
MaBling <akck0918@gmail.com>
2023-08-21 03:08:02 +0900

    Add lsp keymap

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -43,24 +43,49 @@ return {
 
         -- Buffer local mappings.
         -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local opts = { buffer = ev.buf }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wl', function()
-          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
-        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<space>f', function()
-          vim.lsp.buf.format { async = true }
-        end, opts)
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = ev.buf, })
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = ev.buf, })
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = ev.buf })
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = ev.buf })
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, { buffer = ev.buf })
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = ev.buf })
+
+        local wk = require("which-key")
+        wk.register({
+          ["<leader>"] = {
+            l = {
+              name = "+lsp",
+
+              a = {
+                name = "+code actions",
+
+                a = { vim.lsp.buf.code_action, "code actions" }
+              },
+              g = {
+                name = "+goto",
+
+                r = { vim.lsp.buf.references, "find references" },
+                t = { vim.lsp.buf.type_definition, "find type definitions" }
+              },
+              r = {
+                name = "+refactor",
+
+                r = { vim.lsp.buf.rename, "rename" }
+              },
+              w = {
+                name = "+workspaces",
+
+                l = { function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, "list workspace folders" }
+              },
+              ["="] = {
+                name = "+formatting",
+
+                ["="] = { function () vim.lsp.buf.format { async = true } end, "format buffer" }
+              }
+            }
+          }
+        }, { buffer = ev.buf })
+
       end,
     })
   end
