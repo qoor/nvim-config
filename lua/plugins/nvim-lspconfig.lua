e2a4cc683ed2d675de788b643ce0e63deeda6120
MaBling <akck0918@gmail.com>
2023-08-23 23:27:43 +0900

    Add `clangd` language server

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -32,6 +32,8 @@ return {
       },
     }
 
+    lspconfig.clangd.setup {}
+
     -- Use LspAttach autocommand to only map the following keys
     -- after the language server attaches to the current buffer
     vim.api.nvim_create_autocmd('LspAttach', {
