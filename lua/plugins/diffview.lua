f6322ed95bcebb8efe9881e4e33f8e037cbfd58a
MaBling <akck0918@gmail.com>
2023-08-28 01:23:20 +0900

    Add `diffview.nvim` keymap

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -2,7 +2,23 @@ return {
   "sindrets/diffview.nvim",
   lazy = false,
   command = "DiffviewOpen",
+  dependencies = {
+    "which-key.nvim"
+  },
   init = function()
     vim.opt.fillchars:append { diff = "╱" }
+
+    local wk = require("which-key")
+    wk.register({
+      ["<leader>"] = {
+        d = {
+          name = "+diffview",
+
+          d = { "<cmd>DiffviewOpen<cr>", "enter diffview" },
+          q = { "<cmd>DiffviewClose<cr>", "quit diffview" }
+        }
+      }
+    })
+
   end
 }
