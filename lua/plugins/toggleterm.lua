e730d685b26f77d94680982e751ad24ce2e3f180
MaBling <akck0918@gmail.com>
2023-09-25 16:21:54 +0900

    Add `toggleterm` plugin

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,9 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  keys = {
+    { "<leader>s", "<cmd>ToggleTerm<CR>", desc = "Toggle shell" },
+    { "<ESC>", "<C-\\><C-n>", mode = "t", noremap = true },
+  },
+  opts = {},
+}
