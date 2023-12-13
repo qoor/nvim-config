7468cbe07c82da61b1a91f809b0fc9e20688c83e
MaBling <akck0918@gmail.com>
2023-12-13 20:44:50 +0900

    Automatically open/close DAP UI

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -84,6 +84,15 @@ return {
     })
 
     dapui.setup()
+    dap.listeners.after.event_initialized["dapui_config"] = function ()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated["dapui_config"] = function ()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited["dapui_config"] = function ()
+      dapui.close()
+    end
 
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('UserDapConfig', {}),
