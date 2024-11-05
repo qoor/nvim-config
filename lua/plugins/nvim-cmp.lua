221691f9124feaa4c3a85ff89d0133be6709529a
MaBling <akck0918@gmail.com>
2024-11-06 08:34:17 +0900

    Unmap `<TAB>` key from nvim-cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -46,8 +46,8 @@ return {
             -- they way you will only jump inside the snippet region
           elseif luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
+          -- elseif has_words_before() then
+          --   cmp.complete()
           else
             fallback()
           end
