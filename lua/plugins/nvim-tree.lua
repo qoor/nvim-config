0fe348c97432e985662151ea8812b11154cb50dc
MaBling <akck0918@gmail.com>
2024-11-12 22:44:31 +0900

    Fix nvim-tree bad behavior

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,19 +1,20 @@
 local function open_nvim_tree(data)
-
-  -- buffer is a [No Name]
-  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
-
   -- buffer is a directory
   local directory = vim.fn.isdirectory(data.file) == 1
 
-  if not no_name and not directory then
+  if not directory then
+
     return
   end
 
+  -- create a new, empty buffer
+  vim.cmd.enew()
+
+  -- wipe the directory buffer
+  vim.cmd.bw(data.buf)
+
   -- change to the directory
-  if directory then
-    vim.cmd.cd(data.file)
-  end
+  vim.cmd.cd(data.file)
 
   -- open the tree
   require("nvim-tree.api").tree.open()
@@ -101,6 +102,25 @@ return {
           for _, w in ipairs(tree_wins) do
             vim.api.nvim_win_close(w, true)
           end
+        end
+      end
+    })
+    vim.api.nvim_create_autocmd("BufEnter", {
+      nested = true,
+      callback = function()
+        local api = require('nvim-tree.api')
+
+        -- Only 1 window with nvim-tree left: we probably closed a file buffer
+        if #vim.api.nvim_list_wins() == 1 and api.tree.is_tree_buf() then
+          -- Required to let the close event complete. An error is thrown without this.
+          vim.defer_fn(function()
+            -- close nvim-tree: will go to the last hidden buffer used before closing
+            api.tree.toggle({find_file = true, focus = true})
+            -- re-open nivm-tree
+            api.tree.toggle({find_file = true, focus = true})
+            -- nvim-tree is still the active window. Go to the previous window.
+            vim.cmd("wincmd p")
+          end, 0)
         end
       end
     })
