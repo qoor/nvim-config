e64fdd4cd11a16030ec61937bc9debc4250bc83d
MaBling <akck0918@gmail.com>
2024-12-09 14:10:15 +0900

    Add call hierarchy keymaps

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -1,6 +1,5 @@
 return {
   "nvimdev/lspsaga.nvim",
-  event = "LspAttach",
   dependencies = {
     "neovim/nvim-lspconfig",
   },
@@ -14,7 +13,7 @@ return {
         keys = {
           quit = "q"
         }
-      }
+      },
     })
 
     vim.api.nvim_create_autocmd('LspAttach', {
@@ -25,6 +24,8 @@ return {
 
           { "<leader>laa", "<cmd>:Lspsaga code_action<cr>", desc = "code actions" },
           { "<leader>lrr", "<cmd>:Lspsaga rename<cr>", desc = "rename" },
+          { "<leader>lgh", "<cmd>:Lspsaga incoming_calls<cr>", desc = "show incoming call hierarchy" },
+          { "<leader>lgo", "<cmd>:Lspsaga outgoing_calls<cr>", desc = "show outgoing call hierarchy" },
         })
       end
     })
