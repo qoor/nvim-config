8b6b376349d16ea6828e5bbea1aa3d37a1fd1bd3
MaBling <akck0918@gmail.com>
2025-05-11 01:48:13 +0900

    add: Esc key to nohlsearch

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -5,4 +5,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<C-h>", "<CMD>bp<CR>", { silent = true })
 vim.keymap.set("n", "<C-l>", "<CMD>bn<CR>", { silent = true })
 vim.keymap.set("n", "<C-`>", "<CMD>b#<CR>", { silent = true })
+vim.keymap.set("n", "<esc>", "<CMD>:nohl<CR>", { silent = true, noremap = true })
+
 --vim.keymap.set("n", "<C-d>", "<CMD>b# <BAR> bd #<CR>", { silent = true })
