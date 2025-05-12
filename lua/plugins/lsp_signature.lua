91cdf74b14eb5bf45617f4e92350a2a3a6a088d7
MaBling <akck0918@gmail.com>
2025-05-13 00:08:55 +0900

    fix: Update config

--- lua/plugins/lsp_signature.lua
+++ lua/plugins/lsp_signature.lua
@@ -3,9 +3,10 @@ return {
   event = "InsertEnter",
   opts = {
     bind = true,
-    -- handler_opts = {
-    --   border = "rounded"
-    -- }
+    max_height = 64,
+    handler_opts = {
+      border = "single"   -- double, rounded, single, shadow, none, or a table of borders
+    },
   },
   -- or use config
   -- config = function(_, opts) require'lsp_signature'.setup({you options}) end
