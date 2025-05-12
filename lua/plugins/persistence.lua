a0099e6dbf3522fee18850281ae402d8898c74c0
MaBling <akck0918@gmail.com>
2025-05-12 23:18:26 +0900

    fix: Change config

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -1,16 +1,24 @@
 return {
   "folke/persistence.nvim",
-  event = "BufReadPre",
-  keys = {
-    { "<leader>q", group = "+session" },
-    -- load the session for the current directory
-    { "<leader>qq", function () require("persistence").load() end, desc = "load session" },
-    -- select a session to load
-    { "<leader>qs", function () require("persistence").select() end, desc = "select session" },
-    -- load the last session
-    { "<leader>ql", function () require("persistence").load({ last = true }) end, desc = "load last session" },
-    -- stop Persistence => session won't be saved on exit
-    { "<leader>qd", function () require("persistence").stop() end, desc = "do not save on exit" },
+  dependencies = {
+    "folke/which-key.nvim",
   },
-  opts = {}
+  event = "BufReadPre",
+  opts = {},
+  config = function(_, opts)
+    require("persistence").setup(opts)
+
+    local wk = require("which-key")
+    wk.add({
+      { "<leader>q", group = "+session" },
+      -- load the session for the current directory
+      { "<leader>qq", function () require("persistence").load() end, desc = "load session" },
+      -- select a session to load
+      { "<leader>qs", function () require("persistence").select() end, desc = "select session" },
+      -- load the last session
+      { "<leader>ql", function () require("persistence").load({ last = true }) end, desc = "load last session" },
+      -- stop Persistence => session won't be saved on exit
+      { "<leader>qd", function () require("persistence").stop() end, desc = "do not save on exit" },
+    })
+  end
 }
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,15 +1,21 @@
 return {
   "folke/trouble.nvim",
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
-  cmd = "Trouble",
-  keys = {
-    { "<leader>x", group = "+problem", },
-    { "<leader>xd", "<cmd>Trouble diagnostics toggle<cr>", desc = "show problems", },
-    { "<leader>xb", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "show current buffer problems", },
-    { "<leader>xs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "show problem symbols", },
-    { "<leader>xw", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "show workspace problems", },
-    { "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "show location list", },
-    { "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "show quickfix list",
-    },
+  dependencies = {
+    "folke/which-key.nvim",
   },
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  config = function(_, opts)
+    require("trouble").setup(opts)
+
+    local wk = require("which-key")
+    wk.add({
+      { "<leader>x", group = "+problem" },
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "show problems" },
+      { "<leader>xb", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "show current buffer problems" },
+      { "<leader>xs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "show problem symbols" },
+      { "<leader>xw", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "show workspace problems" },
+      { "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "show location list" },
+      { "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "show quickfix list" },
+    })
+  end
 }
