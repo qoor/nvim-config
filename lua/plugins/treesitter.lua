5eef3616f00e372acd328e7d6bb9a801605b173f
MaBling <akck0918@gmail.com>
2025-08-20 08:18:40 +0900

    fix: Improve nvim-treesitter config readability

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,10 +3,9 @@ return {
   build = function()
     require("nvim-treesitter.install").update({ with_sync = true })
   end,
-  config = function()
-    require("nvim-treesitter.configs").setup {
-      highlight = { enable = true },
-      ensure_installed = { "vimdoc", "luadoc", "vim", "lua", "markdown", "json", "c", "cpp", "rust", "python" },
-    }
-  end
+  main = "nvim-treesitter.configs",
+  opts = {
+    highlight = { enable = true },
+    ensure_installed = { "vimdoc", "luadoc", "vim", "lua", "markdown", "json", "c", "cpp", "rust", "python" },
+  }
 }
