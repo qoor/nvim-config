ae831baaa515cef957c2d5aff4b0cee53c14cdb3
MaBling <akck0918@gmail.com>
2025-08-27 08:30:08 +0900

    feat: Add some keymaps

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -2,6 +2,14 @@ vim.keymap.set("n", "<Space>", "<Nop>", { silent = true })
 
 vim.g.mapleader = " "
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
+
+vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
 vim.keymap.set("n", "<C-h>", "<CMD>bp<CR>", { silent = true })
 vim.keymap.set("n", "<C-l>", "<CMD>bn<CR>", { silent = true })
 vim.keymap.set("n", "<C-`>", "<CMD>b#<CR>", { silent = true })
