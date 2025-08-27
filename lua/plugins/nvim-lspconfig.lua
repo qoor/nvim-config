f73366b930f7a0875153f1e880bf71ef64c55b3f
MaBling <akck0918@gmail.com>
2025-08-28 08:29:35 +0900

    fix: Set incorrect LSP capabilities

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -10,7 +10,12 @@ return {
     "nvim-telescope/telescope.nvim"
   },
   config = function()
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            require('cmp_nvim_lsp').default_capabilities())
+    vim.lsp.protocol.make_client_capabilities()
 
     local servers = { "lua_ls", "rust_analyzer", "clangd", "cmake" }
 
