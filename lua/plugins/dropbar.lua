459858930081a220c0382ffc5475f567f386f2ce
MaBling <akck0918@gmail.com>
2025-08-29 08:24:06 +0900

    fix: dropbar deps

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -1,8 +1,10 @@
 return {
   "Bekaboo/dropbar.nvim",
   dependencies = {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make"
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make"
+    },
   },
   config = function()
     local dropbar_api = require('dropbar.api')
