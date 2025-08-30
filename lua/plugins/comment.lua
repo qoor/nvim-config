ba4500bebdc6b9d6cce0726970a40247aead73a4
MaBling <akck0918@gmail.com>
2025-08-31 07:59:01 +0900

    fix: Do not show todo-comments signs

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -12,6 +12,7 @@ return {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
+      signs = false, -- show icons in the signs column
       highlight = {
         keyword = "bg",
         after = "",
