7b7f0244eff095b1c322d93412d30abc3b634522
MaBling <akck0918@gmail.com>
2025-08-31 00:17:38 +0900

    fix: Change LuaSnip config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -272,23 +272,21 @@ return {
     -- these dependencies will only be loaded when cmp loads
     -- dependencies are always lazy-loaded unless specified otherwise
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-      "onsails/lspkind.nvim",
-      "folke/lazydev.nvim",
-
       {
         "L3MON4D3/LuaSnip",
         -- follow latest release.
         version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
         -- install jsregexp (optional!).
         build = "make install_jsregexp",
-        lazy = true
       },
+
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "saadparwaiz1/cmp_luasnip",
+      "onsails/lspkind.nvim",
+      "folke/lazydev.nvim",
     },
     config = function()
       local cmp = require("cmp")
