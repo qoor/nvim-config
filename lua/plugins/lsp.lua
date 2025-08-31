b2edfdfaa8cf5bb1625e488b813c49b32dd3fd7d
MaBling <akck0918@gmail.com>
2025-09-01 08:31:10 +0900

    fix: Remove unnecessary LSP capabilities

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -81,15 +81,7 @@ return {
     },
     config = function()
       local capabilities = vim.tbl_deep_extend(
-        "force",
-        {
-          textDocument = {
-            foldingRange = {
-              dynamicRegistration = false,
-              lineFoldingOnly = true
-            }
-          }
-        },
+        "force", {},
         vim.lsp.protocol.make_client_capabilities(),
         require('cmp_nvim_lsp').default_capabilities())
 
