6ec2648d3c983defc16975fa49c2810ca53e83d1
MaBling <akck0918@gmail.com>
2025-10-21 08:17:26 +0900

    feat: Add some keymaps description

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -8,10 +8,10 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "J", "mzJ`z")
 
-vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set({ "n", "v" }, "<leader>e", "\"_d")
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste without copy" })
+vim.keymap.set({ "n", "v" }, "<leader>e", "\"_d", { desc = "Delete without copy" })
 
-vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace word" })
 
 if not vim.g.vscode then
   vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
