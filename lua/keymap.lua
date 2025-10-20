e5ee20af232391bfd8149b0c2648f3df95dde0a4
MaBling <akck0918@gmail.com>
2025-10-20 22:21:58 +0900

    fix: Remove VSCode keymaps (Move lines up / down)

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -8,15 +8,15 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "J", "mzJ`z")
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
 
 vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 if not vim.g.vscode then
+  vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+  vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
   vim.keymap.set("n", "<C-h>", "<CMD>bp<CR>", { silent = true })
   vim.keymap.set("n", "<C-l>", "<CMD>bn<CR>", { silent = true })
   vim.keymap.set("n", "<C-`>", "<CMD>b#<CR>", { silent = true })
