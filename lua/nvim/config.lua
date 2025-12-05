53d1dc6caaea6393cfa980287d3e75fcfb181f60
MaBling <akck0918@gmail.com>
2025-12-05 10:31:46 +0900

    Revert "feat: Add trailing whitespace char config"
    This reverts commit c121e0e868faea76d3ea834ffe2685613ab4945a.

--- lua/nvim/config.lua
+++ lua/nvim/config.lua
@@ -38,11 +38,6 @@ vim.opt.cino = "N-s,g0"
 -- delays and poor user experience.
 vim.opt.updatetime = 50
 
-vim.opt.list = true
-vim.opt.listchars = {
-  trail = "·"
-}
-
 if vim.env.SSH_TTY then
   vim.g.clipboard = "osc52"
 end
