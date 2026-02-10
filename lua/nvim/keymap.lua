190d7f1c97f5961b317e12d0bf698eb15a1fccd4
MaBling <akck0918@gmail.com>
2026-02-10 09:10:01 +0900

    feat: Add tmux-sessionizer keymap

--- lua/nvim/keymap.lua
+++ lua/nvim/keymap.lua
@@ -2,3 +2,4 @@
   vim.keymap.set("n", "<C-l>", "<CMD>bn<CR>", { silent = true })
   vim.keymap.set("n", "<C-`>", "<CMD>b#<CR>", { silent = true })
 
+  vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
