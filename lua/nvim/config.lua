25b225a5b3cb38842d0d21cdd098924a80e60565
MaBling <akck0918@gmail.com>
2026-02-27 14:39:58 +0900

    fix: Use netrw by default

--- lua/nvim/config.lua
+++ lua/nvim/config.lua
@@ -1,6 +1,9 @@
 -- disable netrw
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
 
 --vim.cmd("language en_US")
 
