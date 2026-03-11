375792e9c85c02943ba4214e8fa44107127eba85
MaBling <akck0918@gmail.com>
2026-03-12 08:10:07 +0900

    fix: Added highlight on yank

--- lua/shared/config.lua
+++ lua/shared/config.lua
@@ -12,3 +12,14 @@ vim.opt.smartindent = true
 vim.schedule(function()
   vim.opt.clipboard = "unnamedplus"
 end)
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('HighlightYank', {}),
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',
+      timeout = 40,
+    })
+  end,
+})
