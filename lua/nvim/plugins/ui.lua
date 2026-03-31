ea03ed5977d5ef48cea802ef859f56d3226f7031
MaBling <akck0918@gmail.com>
2026-03-31 21:18:10 +0900

    feat: Add friendly-snippet

--- lua/nvim/plugins/ui.lua
+++ lua/nvim/plugins/ui.lua
@@ -428,6 +428,7 @@ return {
     dependencies = {
       {
         "L3MON4D3/LuaSnip",
+        dependencies = { "rafamadriz/friendly-snippets" },
         -- follow latest release.
         version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
         -- install jsregexp (optional!).
